3cbf3fdf1061448c073a8a6d3fd649d7a6809e81
Kolosov Nikita <87139155+NKolosov097@users.noreply.github.com>
2023-02-20 17:42:07 +0300

    add: 03-slider project

---
+++ Марафон 5 проектов за 5 дней/03-slider/app.js
@@ -0,0 +1,45 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+function changeSlide (direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+
+    const height = container.clientHeight
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${slidesCount - 1}00vh`
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up')
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down')
+    }
+})
